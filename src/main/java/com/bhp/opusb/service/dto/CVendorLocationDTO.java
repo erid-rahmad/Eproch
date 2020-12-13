34ac5b49fba5e54278df6a9d725c92a2a247cd71
Ananta Aryadewa <ananta.aryadewa@berca.co.id>
2020-12-14 01:32:33 +0700

    BEP-29 Fix vendor location dto
    Add address 1-4 & postal code getters

--- src/main/java/com/bhp/opusb/service/dto/CVendorLocationDTO.java
+++ src/main/java/com/bhp/opusb/service/dto/CVendorLocationDTO.java
@@ -148,20 +148,40 @@ public class CVendorLocationDTO extends AbstractAuditingDTO {
         return address.toString();
     }
 
+    public String getAddress1() {
+        return address1;
+    }
+
     public void setAddress1(String address1) {
         this.address1 = address1;
+    }
+
+    public String getAddress2() {
+        return address2;
     }
 
     public void setAddress2(String address2) {
         this.address2 = address2;
     }
 
+    public String getAddress3() {
+        return address3;
+    }
+
     public void setAddress3(String address3) {
         this.address3 = address3;
     }
 
+    public String getAddress4() {
+        return address4;
+    }
+
     public void setAddress4(String address4) {
         this.address4 = address4;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
     }
 
     public void setPostalCode(String postalCode) {
