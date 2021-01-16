9b2d3567cc8c1fd2a3d09e434d19ef7ac265870e
Ananta Aryadewa <ananta.aryadewa@gmail.com>
2021-01-16 12:06:37 +0700

    BEP-20 Fix Business Cat Step
    Some records didn't show the level 3 categories.

--- src/main/webapp/app/utils/form.ts
+++ src/main/webapp/app/utils/form.ts
@@ -2,57 +2,13 @@ import { ADColumnType } from '@/shared/model/ad-column.model';
 import { IADField } from '@/shared/model/ad-field.model';
 import { isStringField } from "@/utils/validate";
 
-export const buildCascaderOptions = source => {
-  let parents = new Map();
-  let children = new Map();
-  let result = [];
-
-  const addToChildrenCache = item => {
-    let items = children.get(item.parent);
-    if (!items) {
-      items = [];
-      children.set(item.parent, items);
-    }
-    items.push(item);
-  };
-
-  const addChild = (parent, item) => {
-    if (!parent.children) {
-      parent.children = [];
-    }
-    parent.children.push(item);
-  };
-
-  const addChildren = item => {
-    let items = children.get(item.value);
-    if (items) {
-      if (!item.children) {
-        item.children = [];
-      }
-      item.children.concat(items);
-    }
-  };
-
-  source.forEach(s => {
-    let item = { ...s };
-    if (item.parent) {
-      let parent = parents.get(item.parent);
-      if (!parent) {
-        addToChildrenCache(item);
-      } else {
-        addChild(parent, item);
-      }
-
-      addChildren(item);
-    } else {
-      result.push(item);
-      addChildren(item);
-    }
-    parents.set(item.value, item);
-    delete item.parent;
-  });
-
-  return result;
+export const buildCascaderOptions = (source: any[]) => {
+  return source
+    .reduce((p, currentItem, i, sourceArray) => {
+      let parent = !!currentItem.parent && sourceArray.find(e => e.value === currentItem.parent);
+      !!parent ? !!parent.children && parent.children.push(currentItem) || (parent.children = [currentItem]) : p.push(currentItem);
+      return p;
+    }, []);
 };
 
 export const normalizeField = (record: Record<string, any>, field: IADField) => {
