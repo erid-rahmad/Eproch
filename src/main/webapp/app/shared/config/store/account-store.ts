bab40af2b5afb9718c85327f609cb6c5bed2841d
Ananta Aryadewa <ananta.aryadewa@gmail.com>
2021-03-16 13:08:56 +0700

    Provides more detailed user info in accountStore

--- src/main/webapp/app/shared/config/store/account-store.ts
+++ src/main/webapp/app/shared/config/store/account-store.ts
@@ -27,18 +27,40 @@ class AccountStore extends VuexModule implements IAccountState {
   public authorities = new Set<string>();
   public grantedDocActions = new Map<number, Set<number>>();
   public grantedDashboards = new Map<string, Set<number>>();
-
-  // TODO Get organization ID from userIdentity.
-  public properties = new Map<string, any>([
-    ['adOrganizationId', 1]
-  ]);
+  public properties = new Map<string, any>();
 
   public get account() {
     return this.userIdentity;
   }
 
+  public get isEmployee() {
+    return this.userDetails.employee;
+  }
+
+  public get isVendor() {
+    return this.userDetails.vendor;
+  }
+
+  public get organizationInfo() {
+    return {
+      id: this.userDetails.adOrganizationId,
+      name: this.userDetails.adOrganizationName
+    };
+  }
+
   public get property() {
     return (key: string) => this.properties.get(key);
+  }
+
+  public get vendorInfo() {
+    if (! this.isVendor) {
+      return {};
+    }
+    
+    return {
+      id: this.userDetails.cVendorId,
+      name: this.userDetails.cVendorName
+    };
   }
 
   @Mutation
@@ -57,7 +79,13 @@ class AccountStore extends VuexModule implements IAccountState {
   @Mutation
   private SET_USER_DETAILS(user: any) {
     this.userDetails = user;
+    this.properties.set('adOrganizationId', user.adOrganizationId);
+    this.properties.set('adOrganizationName', user.adOrganizationName);
+    this.properties.set('email', user.email);
+    this.properties.set('isVendor', user.vendor);
     this.properties.set('userLogin', user.userLogin);
+    this.properties.set('vendorId', user.cVendorId);
+    this.properties.set('vendorName', user.cVendorName);
   }
 
   @Mutation
