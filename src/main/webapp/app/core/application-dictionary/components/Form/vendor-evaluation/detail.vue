90ab60d4b7b08f8d258431e5ae12cdbe2f090177
Ananta Aryadewa <ananta.aryadewa@gmail.com>
2021-04-05 12:16:02 +0700

    AP-387 Update vendor evaluation form
    Update the mock-up data and rename the files

---
+++ src/main/webapp/app/core/application-dictionary/components/Form/vendor-evaluation/detail.vue
@@ -0,0 +1,211 @@
+<template>
+  <div class="vendor-evaluation-detail">
+    <el-form
+      ref="mainForm"
+      label-position="left"
+      label-width="150px"
+      :model="data"
+      :rules="validationSchema"
+      size="mini"
+    >
+      <el-row :gutter="24">
+        <el-col :span="8">
+          <el-form-item
+            label="Evaluation No."
+            prop="evaluationNo"
+          >
+            <el-input
+              v-model="data.evaluationNo"
+              class="form-input"
+              clearable
+            ></el-input>
+          </el-form-item>
+          <el-form-item
+            label="Reviewer"
+            prop="reviewer"
+          >
+            <el-select
+              v-model="data.reviewer"
+              class="form-input"
+              clearable
+              filterable
+              placeholder="Select a Reviewer"
+              style="width: 100%"
+            >
+              <el-option
+                v-for="item in reviewers"
+                :key="item.id"
+                :label="item.name"
+                :value="item.id"
+              />
+            </el-select>
+          </el-form-item>
+
+          <el-form-item
+            label="Vendor Name"
+            prop="vendorId"
+          >
+            <el-select
+              v-model="data.vendorId"
+              class="form-input"
+              clearable
+              filterable
+              placeholder="Select a Vendor"
+              style="width: 100%"
+            >
+              <el-option
+                v-for="item in vendorOptions"
+                :key="item.id"
+                :label="item.name"
+                :value="item.id"
+              />
+            </el-select>
+          </el-form-item>
+
+          <el-form-item
+            label="Aggreement No."
+            prop="aggreementNo"
+          >
+            <el-input
+              ref="aggreementNo"
+              v-model="data.aggreementNo"
+              clearable
+              placeholder="Please Enter Aggreement No."
+              @change="onAggreementChanged"
+            >
+              <el-button
+                icon="el-icon-search"
+                slot="append"
+              >
+                Search
+              </el-button>
+            </el-input>
+          </el-form-item>
+        </el-col>
+        <el-col :span="8">
+          <el-form-item
+            label="Evaluation Type"
+            prop="evaluationType"
+          >
+            <el-input
+              v-model="data.evaluationType"
+              class="form-input"
+              clearable
+              disabled
+            ></el-input>
+          </el-form-item>
+          <el-form-item
+            label="Evaluation Period"
+            prop="evaluationPeriod"
+          >
+            <el-input
+              v-model="data.evaluationPeriod"
+              class="form-input"
+              clearable
+              disabled
+            ></el-input>
+          </el-form-item>
+          <el-form-item
+            label="Evaluation Date"
+            prop="evaluationDate"
+          >
+            <el-date-picker
+              v-model="data.evaluationDate"
+              class="form-input"
+              clearable
+              disabled
+              :format="dateDisplayFormat"
+              placeholder="Pick a date"
+              size="mini"
+              style="width: 100%"
+              type="date"
+              :value-format="dateValueFormat"
+            ></el-date-picker>
+          </el-form-item>
+        </el-col>
+        <el-col :span="8">
+          <el-form-item
+            label="Total Score"
+            prop="totalScore"
+          >
+            <el-input-number
+              v-model="data.totalScore"
+              class="form-input"
+              clearable
+              controls-position="right"
+              disabled
+              :precision="2"
+            ></el-input-number>
+          </el-form-item>
+        </el-col>
+      </el-row>
+    </el-form>
+    <el-row>
+      <el-col
+        :xs="24"
+        :sm="12"
+      >
+        <el-table
+          ref="evaluationLines"
+          border
+          class="evaluation-line-table"
+          :data="data.evaluationLines"
+          highlight-current-row
+          :max-height="gridSchema.maxHeight"
+          size="mini"
+          stripe
+        >
+
+          <el-table-column
+            label="Question Category"
+            width="150"
+            prop="cQuestionCategoryName"
+            show-overflow-tooltip
+          ></el-table-column>
+
+          <el-table-column
+            label="Question"
+            width="200"
+            prop="question"
+            show-overflow-tooltip
+          ></el-table-column>
+
+          <el-table-column
+            label="Score"
+            width="150"
+          >
+            <template slot-scope="{ row }">
+              <el-rate
+                v-model="row.rate"
+                :colors="['#99A9BF', '#F7BA2A', '#FF9900']"
+                @change="onRateChanged"
+              ></el-rate>
+            </template>
+          </el-table-column>
+
+          <el-table-column
+            label="Remark"
+            min-width="200"
+          >
+            <template slot-scope="{ row }">
+              <el-input
+                v-model="row.remark"
+                class="remark"
+                clearable
+                :maxlength="255"
+                size="mini"
+              ></el-input>
+            </template>
+          </el-table-column>
+        </el-table>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+<script lang="ts" src="./index.component.ts"></script>
+
+<style lang="scss" scoped>
+.evaluation-line-table .el-input.remark {
+  margin: 4px 0;
+}
+</style>
--- src/main/webapp/app/core/application-dictionary/components/Form/vendor-evaluation/index.component.ts
+++ src/main/webapp/app/core/application-dictionary/components/Form/vendor-evaluation/index.component.ts
@@ -1,149 +1,94 @@
 import AccessLevelMixin from '@/core/application-dictionary/mixins/AccessLevelMixin';
-import settings from '@/settings';
-import { Component, Mixins, Vue, Inject } from 'vue-property-decorator';
+import { ElTable } from 'element-ui/types/table';
+import Vue from 'vue';
+import Component, { mixins } from 'vue-class-component';
+import { Inject } from 'vue-property-decorator';
 import DynamicWindowService from '../../DynamicWindow/dynamic-window.service';
+import VendorEvaluationDetail from './detail.vue';
 
 const VendorEvaluationProp = Vue.extend({
   props: {
+    approval: Boolean
   }
-});
+})
 
-@Component
-export default class VendorEvaluation extends Mixins(AccessLevelMixin, VendorEvaluationProp) {
+@Component({
+  components: {
+    VendorEvaluationDetail
+  }
+})
+export default class VendorEvaluation extends mixins(AccessLevelMixin, VendorEvaluationProp) {
 
   @Inject('dynamicWindowService')
-  protected commonService: (baseApiUrl: string) => DynamicWindowService;
+  private commonService: (baseApiUrl: string) => DynamicWindowService;
 
-  // Mockup Aggrement data.
-  aggreements = new Map([
-    ['AG-20210330', {
-      documentNo: 'AG-20210330',
-      evaluationType: 'Evaluation Type 103',
-      evaluationPeriod: 'Quarterly',
-      evaluationDate: new Date()
-    }],
-    ['AG-20210329', {
-      documentNo: 'AG-20210329',
-      evaluationType: 'Evaluation Type 102',
-      evaluationPeriod: 'Yearly',
-      evaluationDate: new Date()
-    }],
-    ['AG-20210328', {
-      documentNo: 'AG-20210328',
-      evaluationType: 'Evaluation Type 101',
-      evaluationPeriod: 'End of Aggreement',
-      evaluationDate: new Date()
-    }]
-  ]);
+  index = true;
+  selectedRow: any = {};
+  selectedDocumentAction: any = {};
+  showDocumentActionConfirm = false;
 
-  data = {
-    documentNo: null,
-    reviewer: null,
-    vendorId: null,
-    aggreementNo: null,
-    evaluationType: null,
-    evaluationPeriod: null,
-    evaluationDate: null,
-    totalScore: null,
-    evaluationLines: []
+  documentStatuses = {
+    APV: 'Approved',
+    DRF: 'Draft',
+    RJC: 'Rejected',
+    RVS: 'Revised',
+    SMT: 'Submitted',
   }
 
-  gridSchema = {
-    defaultSort: {},
-    emptyText: 'No Records Found',
-    maxHeight: 256,
-    height: 200
-  };
-
-  validationSchema = {};
-  reviewers = [];
-  vendorOptions = [];
-
-  get dateDisplayFormat() {
-    return settings.dateDisplayFormat;
-  }
-
-  get dateValueFormat() {
-    return settings.dateValueFormat;
-  }
-
-  onAggreementChanged(documentNo?: string) {
-    if (!documentNo) {
-      this.$set(this.data, 'evaluationType', null);
-      this.$set(this.data, 'evaluationPeriod', null);
-      this.$set(this.data, 'evaluationDate', null);
+  vendorEvaluations = [
+    {
+      aggreementNo: '13334',
+      aggreementTitle: 'Pengadaan Kendaraan Operasional',
+      evaluationDate: new Date('2021-03-31'),
+      totalScore: 3.67,
+      documentStatus: 'APV',
     }
+  ];
 
-    const aggreement = this.aggreements.get(documentNo);
-    if (aggreement) {
-      this.$set(this.data, 'evaluationType', aggreement.evaluationType);
-      this.$set(this.data, 'evaluationPeriod', aggreement.evaluationPeriod);
-      this.$set(this.data, 'evaluationDate', aggreement.evaluationDate);
-
-      this.retrieveLines(aggreement.evaluationType);
-    }
+  get defaultDocumentAction() {
+    return this.selectedRow.documentAction || 'DRF';
   }
 
-  onRateChanged(_rate: number) {
-    const totalRates = this.data.evaluationLines
-      .map((line: any): number => line.rate || 0)
-      .reduce((prev, next) => prev + next, 0);
-
-    this.$set(this.data, 'totalScore', totalRates / this.data.evaluationLines.length);
+  get documentApproved() {
+    return this.selectedRow.processed && this.selectedRow.approved || false;
   }
 
-  created() {
-    this.retrieveReviewers();
-    this.retrieveVendors();
+  get documentTypeId() {
+    return this.selectedRow.documentTypeId;
   }
 
-  retrieveLines(evaluationType: string) {
-    this.commonService('/api/c-vendor-evaluation-lines')
-      .retrieve({
-        criteriaQuery: this.updateCriteria([
-          'active.equals=true',
-          `cVendorEvaluationName.equals=${evaluationType}`
-        ])
-      })
-      .then(res => {
-        this.$set(this.data, 'evaluationLines', res.data);
-      })
+  get isDraft() {
+    return this.selectedRow.documentStatus === 'DRF' || this.selectedRow.documentStatus === 'RVS';
   }
 
-  retrieveReviewers() {
-    this.commonService('/api/ad-users')
-      .retrieve({
-        criteriaQuery: this.updateCriteria([
-          'active.equals=true',
-          'employee.equals=true'
-        ]),
-        paginationQuery: {
-          page: 0,
-          size: 1000,
-          sort: ['userLogin']
-        }
-      })
-      .then(res => {
-        this.reviewers = res.data;
-      });
+  onDocumentActionChanged(action: any) {
+    this.selectedDocumentAction = action;
+    this.showDocumentActionConfirm = true;
   }
 
-  retrieveVendors() {
-    this.commonService('/api/c-vendors')
-      .retrieve({
-        criteriaQuery: this.updateCriteria([
-          'active.equals=true',
-          'processed.equals=true',
-          'approved.equals=true'
-        ]),
-        paginationQuery: {
-          page: 0,
-          size: 1000,
-          sort: ['name', 'code']
-        }
-      })
-      .then(res => {
-        this.vendorOptions = res.data;
-      });
+  onCurrentRowChanged(row: any) {
+    this.selectedRow = row;
+  }
+
+  mounted() {
+    this.setRow(this.vendorEvaluations[0]);
+  }
+
+  closeDetail() {
+    this.index = true;
+  }
+
+  printStatus(status: string) {
+    return this.documentStatuses[status];
+  }
+
+  private setRow(record: any) {
+    (<ElTable>this.$refs.mainGrid).setCurrentRow(record);
+  }
+
+  viewDetail(row: any) {
+    console.log('selected row:', row);
+    this.selectedRow = row;
+    this.index = false;
   }
 }
