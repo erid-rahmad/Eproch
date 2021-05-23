a17f6c52108bb5643c09be4e44c2adbc485f7481
Ananta Aryadewa <ananta.aryadewa@gmail.com>
2021-05-24 05:31:36 +0700

    AP-423 Fix pre-bid meeting to submission process

--- src/main/java/com/bhp/opusb/repository/MBiddingSubmissionRepository.java
+++ src/main/java/com/bhp/opusb/repository/MBiddingSubmissionRepository.java
@@ -1,14 +1,18 @@
 package com.bhp.opusb.repository;
 
+import java.util.Optional;
+
+import com.bhp.opusb.domain.CVendor;
+import com.bhp.opusb.domain.MBidding;
 import com.bhp.opusb.domain.MBiddingSubmission;
 
-import org.springframework.data.jpa.repository.*;
 import org.springframework.stereotype.Repository;
 
 /**
  * Spring Data  repository for the MBiddingSubmission entity.
  */
-@SuppressWarnings("unused")
 @Repository
 public interface MBiddingSubmissionRepository extends GenericDocumentRepository<MBiddingSubmission, Long> {
+
+  Optional<MBiddingSubmission> findFirstByBiddingAndVendor(MBidding mBidding, CVendor vendor);
 }
